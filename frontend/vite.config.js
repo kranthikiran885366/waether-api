f878a0437205ae7de33de91f6f58e199d2ae826a
Mallela kranthi Kiran <Mallelakranthikiran@gmail.com>
2025-10-18 08:52:15 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,23 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+ import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
+    port: 10000,
+    allowedHosts: ['waether-api-1.onrender.com'],
     proxy: {
-      '/api': 'http://localhost:4000'
+      '/api': {
+        target: 'https://waether-api-1.onrender.com', // your backend Render URL
+        changeOrigin: true,
+        secure: true
+      }
     }
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 10000,
+    allowedHosts: ['waether-api-1.onrender.com']
   }
-});
+})
